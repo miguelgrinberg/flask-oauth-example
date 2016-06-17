1b84cb7d3cd570c655017e50d84709c3246a6f36
Mike Canann <mikecanann@gmail.com>
2016-06-17 11:40:21 -0400

    updated the flask login import

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, url_for, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask.ext.login import LoginManager, UserMixin, login_user, logout_user,\
+from flask_login import LoginManager, UserMixin, login_user, logout_user,\
     current_user
 from oauth import OAuthSignIn
 
