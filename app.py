1ee45bdb67b6239724793348766e448e4d9ea0d6
Timothy Phan <timothyphan@Timothys-MacBook-Air.local>
2016-12-31 01:56:10 -0500

    added missing flask.flash import in app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user,\
     current_user
